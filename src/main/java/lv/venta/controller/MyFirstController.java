e110be44a6d7582db865ef8092d79e7c03979e1a
baibakv <kbaiba@inbox.lv>
2025-03-31 20:15:16 +0300

    edit: added a new controller

--- src/main/java/lv/venta/controller/MyFirstController.java
+++ src/main/java/lv/venta/controller/MyFirstController.java
@@ -1,14 +1,28 @@
 package lv.venta.controller;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyFirstController {
 	
+	private Random rand = new Random();
 	@GetMapping("/simple") //localhost8080/simple
 	public String myFirstGetController() {
 		System.out.println("Pirmais kotrolieris nostradaja!");
 		return "simple-page"; //paradit simple-page.htm
+	}
+	
+	
+	
+	@GetMapping("/getdata")//localhost8080/getdata
+	public String getControllerSendData(Model model) {
+		System.out.println("Send data kotrolieris nostradaja!");
+		String data =  "Baiba -> " + rand.nextInt(0, 101);
+		model.addAttribute("package", data);
+		return "show-data-page";
 	}
 }
