1ea8ca15951a6ab4fce6c9b44f04aafad9884f2e
baibakv <kbaiba@inbox.lv>
2025-04-01 00:52:07 +0300

    edit- forgot to save lol

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,5 +1,11 @@
 package lv.venta.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,15 +16,29 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 @ToString
+@Table(name = "ProductTable")
+@Entity
 public class Product {
 	//1. mainigie
+	
 	@Setter(value = AccessLevel.NONE)
+	@Column(name = "Id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	
+	@Column(name = "Title")
 	private String title;
+	
+	@Column(name = "Description")
 	private String description;
+	
+	@Column(name = "Price")
 	private float price;
+	
+	@Column(name = "Quantity")
 	private int quantity;
+	
 	//2. getters - no lombok
 	//3. setters - no lombok
 	//4. bezarg - no lombok
