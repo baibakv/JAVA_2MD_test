f3ac71b89fd94e044e160aa459dfa707ee359e9d
baibakv <kbaiba@inbox.lv>
2025-04-01 00:36:07 +0300

    added new controllers

--- src/main/java/lv/venta/controller/MyFirstController.java
+++ src/main/java/lv/venta/controller/MyFirstController.java
@@ -1,10 +1,14 @@
 package lv.venta.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import lv.venta.model.Product;
 
 @Controller
 public class MyFirstController {
@@ -27,4 +31,46 @@ public class MyFirstController {
 		model.addAttribute("package", data);
 		return "show-data-page";
 	}
+	
+	
+	
+	@GetMapping("/getproduct")//localhost:8080/getproduct
+	public String getControllerSendProduct(Model model) {
+		Product newProduct = new Product ("Abols", "Garsigs", 0.99f, 4); 
+		model.addAttribute("package", newProduct);
+		return"show-one-product"; //paradis vienu produktu
+	}
+	
+	
+	
+	@GetMapping("/getmultipleproducts")//localhost:8080/getmultiplsproducts
+	public String getControllerSendMultipleProducts(Model model) {
+		ArrayList<Product>newProducts = new ArrayList<>(Arrays.asList(
+				new Product ("Abols", "Garsigs", 0.99f, 4),
+				new Product ("Gurķis", "Garš", 0.57f, 5),
+				new Product ("Tomats", "Suligs", 1.79f, 1)
+				));
+		model.addAttribute("package", newProducts);
+		return"show-multiple-products";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
